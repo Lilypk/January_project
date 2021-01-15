6cc32bba0a19993e36dd1160bde7c3cde4f5eb48
LILYKLOPCIC <LILYKLOPCIC@GMAIL.COM>
2021-01-15 13:14:06 -0500

    added food, bod, style, decor views

--- January/views.py
+++ January/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from .models import Food, Bod, Style, Decor
 # Create your views here.
+
+def food_list(request):
+    foods = Food.objects.all()
+    return render(request, 'January/food_list.html', {'foods': foods})
+
+def bod_list(request):
+    bods = Bod.objects.all()
+    return render(request, 'January/bod_list.html', {'bods': bods})
+
+def style_list(request):
+    styles = Style.objects.all()
+    return render(request, 'January/style_list.html', {'styles': styles})
+
+def decor_list(request):
+    decors = Decor.objects.all()
+    return render(request, 'January/decor_list.html', {'decors': decors})
+
