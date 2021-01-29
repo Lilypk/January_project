1ca8483ea9e3b21a5c1825fc3e9c0b21f472166e
LILYKLOPCIC <LILYKLOPCIC@GMAIL.COM>
2021-01-29 16:54:34 -0500

    load static files onto the page, base.html

--- January/urls.py
+++ January/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.food_list, name='food_list'),
-    path('/bods', views.bod_list, name='bod_list'),
-    path('/styles', views.style_list, name='style_list'),
-    path('/decors', views.decor_list, name='decor_list'),
+    path('bods/', views.bod_list, name='bod_list'),
+    path('styles/', views.style_list, name='style_list'),
+    path('decors/', views.decor_list, name='decor_list'),
 ]
 
